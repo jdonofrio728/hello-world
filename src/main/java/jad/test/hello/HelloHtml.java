5aa1447ae55942bdf690b656ea28af138f8323ee
Jacob D'Onofrio <jacob.donofrio@autodesk.com>
2018-02-13 15:51:15 -0500

    Change the program to use servlet to output hostname as well. This is useful for this simple app in distributed environments

--- src/main/java/jad/test/hello/HelloHtml.java
+++ src/main/java/jad/test/hello/HelloHtml.java
@@ -1,12 +1,26 @@
 package jad.test.hello;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 public class HelloHtml extends Hello{
 
-	private static final String HTML = "<html><body><h1>Hello World!</h1></body></html";
+	private static final String HTML = "" +
+			"<html>" +
+            "<body>" +
+            "<h1>Hello World!</h1>" +
+            "<h1>${serverName}</h1>" +
+            "</body>" +
+            "</html";
 	
 	@Override
 	public String sayHello() {
-		return HTML;
-	}
+        try {
+            return HTML.replace("${serverName}", Inet4Address.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return HTML.replace("${serverName}", "Error");
+        }
+    }
 
 }
